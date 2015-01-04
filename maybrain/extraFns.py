e9ae47fda13073170170ce70e1d23322d389322e
rittman <tr332@medschl.cam.ac.uk>
2015-01-04 19:14:01 +0000

    Update to local efficiency to allow fully connected graphs to be computationally tractable

--- maybrain/extraFns.py
+++ maybrain/extraFns.py
@@ -43,39 +43,46 @@ def globalefficiency(G, weight=None):
         "Number of nodes <1, can't calculate global efficiency"
         return None
 
-def localefficiency(G, nodes=None, weight=None):
+def localefficiency(G, nodes=None, weight=None, fc=False):
     """
     Returns a dictionary of local efficiency values for each node in the graph.
+    fc is a bit of a bodge, specify this if the graph is fully connected to
+    make it computationally possible to run.
     """    
     # if node list is not specified, use all nodes in the graph
     if not nodes:
         nodes = G.nodes()
+
+    if weight and fc:
+        outDict = {v:efficiencyfunc(v, G, weight=weight)/(len(G.nodes())-1) for v in G.nodes()}
+                
+    else:
+        outDict={}
+
+        for node in nodes:
+
+            Ginodes = nx.neighbors(G,node)
+            Ginodes.append(node)
+            Giedges = G.edges(Ginodes)
     
-    outDict={}
-
-    for node in nodes:
-        Ginodes = nx.neighbors(G,node)
-        Ginodes.append(node)
-        Giedges = G.edges(Ginodes)
-
-        Giedges = [ e for e in Giedges if e[1] in Ginodes ]
-        Giedges = [ e for e in Giedges if e[0] in Ginodes ]
-
-        Gi = nx.Graph()
-        Gi.add_edges_from(Giedges)
-        for e in Gi.edges():
-            Gi.edge[e[0]][e[1]] = G.edge[e[0]][e[1]]
-
-        if Giedges:
-            ssl = [ efficiencyfunc(v,
-                                   Gi,
-                                   weight=weight) for v in Gi.nodes() if not v==node]
-            
-            # correct by number of edges in local graph
-            locEff = np.sum(ssl) / (len(Ginodes)-1)  # -1 because self connections are not expected
-            outDict[node] = locEff
-        else:
-            outDict[node] =  0.
+            Giedges = [ e for e in Giedges if e[1] in Ginodes ]
+            Giedges = [ e for e in Giedges if e[0] in Ginodes ]
+    
+            Gi = nx.Graph()
+            Gi.add_edges_from(Giedges)
+            for e in Gi.edges():
+                Gi.edge[e[0]][e[1]] = G.edge[e[0]][e[1]]
+    
+            if Giedges:
+                ssl = [ efficiencyfunc(v,
+                                       Gi,
+                                       weight=weight) for v in Gi.nodes() if not v==node]
+                
+                # correct by number of edges in local graph
+                locEff = np.sum(ssl) / (len(Ginodes)-1)  # -1 because self connections are not expected
+                outDict[node] = locEff
+            else:
+                outDict[node] =  0.
     
     return(outDict)
     
