57fa4abc83d4bb89d1b1d304db9d153828733c4c
Martyn Rittman <smart3141@netscape.net>
2015-09-17 18:30:47 +0200

    merge issues fixed, especially in bin/gui/GUI.py

--- bin/gui/GUI.py
+++ bin/gui/GUI.py
@@ -5,21 +5,24 @@ GUI for Maybrain
 
 """
 
-#<<<<<<< HEAD
 ## add the local path to python executable path
 #import sys
 #try:
 #    sys.path.append('/home/galileo/Documents/programming/maybrain/maybrain/')
 #except:
 #    pass
+
+
 #=======
+
+
 ## First, and before importing any Enthought packages, set the ETS_TOOLKIT
 ## environment variable to qt4, to tell Traits that we will use Qt.
 #
 #
 ##import os
 ##os.environ['ETS_TOOLKIT'] = 'qt4'
-#>>>>>>> gui_dev
+
 
 # To be able to use PySide or PyQt4 and not run in conflicts with traits,
 # we need to import QtGui and QtCore from pyface.qt
