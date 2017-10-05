85e699b8fa02405274cbd4e38a8d4412b7da3418
tjiagoM <tiago.manuel@gmail.com>
2017-10-05 10:42:37 +0100

    python3 info

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-# Do not execute this file, this is just used for testing purposes
-
 from distutils.core import setup
 
 setup(
     name='Maybrain', 
-    version='0.2.3',
+    version='0.3.0',
     author='Martyn Rittman and Timothy Rittman',
     author_email='mrittman@physics.org',
     packages=['maybrain', 'maybrain.gui'],
 #    scripts=['brainObj.py','extraFns.py', 'highlightObj.py', 'mbplot.py', 'writeFns.py'],
-    url='https://github.com/rittman/maybrain/releases',
+    url='https://github.com/RittmanResearch/maybrain/releases',
     license='LICENSE',
     description='A package for visualizing brain network data',
     long_description=open('README.md').read(),
