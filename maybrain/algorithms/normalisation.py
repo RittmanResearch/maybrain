7cff1262e8db5c9bf1a782c53bfb14975411d396
tjiagoM <tiago.manuel@gmail.com>
2018-02-06 18:22:46 +0000

    normalise_node_wise updated

--- maybrain/algorithms/normalisation.py
+++ maybrain/algorithms/normalisation.py
@@ -1,15 +1,26 @@
+from random import shuffle
 import numpy as np
 import networkx as nx
 from networkx import configuration_model
-from random import shuffle
 
 
-def normalise(G, func, n=500, retNorm=True, inVal=None, weight='weight'):
+from maybrain import constants as ct
+
+#TODO: meter lista de atributos em vez de "spatial
+#TODO indicar que considera que os edges no a têm que ter o attributo 'weight' ja atribuido para cada edge
+#   |-> se calhar meter um teste a ver se o atributo existe, e lançar excepçao se nao?-> meter isto nos testes!
+#TODO: Check inVal is not empty
+#TODO: criar normalise geral para que isto tudo fique reduzido, e no inicio testar o tipo
+
+def normalise(brain, func, n=500, retNorm=True, inVal=None, **kwargs):
     """
     This function normalises the function G by generating a series of n random
     graphs and averaging the results. If retNorm is specified, the normalised
     value is returned, else a list of n values for a random graph are returned.
     """
+    if brain.directed:
+        raise TypeError("normalise() not available for directed graphs")
+            
     vals = []
     for i in range(n):
         rand = configuration_model(list(G.degree().values()))
@@ -49,67 +60,71 @@ def normalise(G, func, n=500, retNorm=True, inVal=None, weight='weight'):
     else: # return a list of the values from the random graph
         return(vals)
 
-def normaliseNodeWise(G, func, inVal={}, n=500, retNorm=True, distance=False, weight='weight', spatial=False):
+def normalise_node_wise(brain, func, init_vals=None, n_iter=500, ret_normalised=True, 
+                        node_attrs=None, edge_attrs=None, **kwargs):
     """
-    This function normalises the function G by generating a series of n random
-    graphs and averaging the results. If retNorm is specified, the normalised
-    value is returned, else a list of n values for a random graph are returned.
+    It normalises measures taken from a brain by generating a series of n random graphs and
+    averaging them.
+
+    brain: an instance of the `Brain` class
+    func: the function that calculates the measure in each node of brain
+    init_vals: the initial measures calculated from brain.G that will be averaged. If this is None,
+               this will be equal to func(brain.G, **kwargs)
+    n_iter: number of iteratios that will be used to generate the random graphs
+    ret_normalised: if True, the normalised measures are returned
+    node_attrs: If the node attributes of brain.G are necessary to a correct calculation of func()
+                in the random graph, just pass the attributes as a list of strings in this parameter
+    edge_attrs: If the edge attributes of brain.G are necessary to a correct calculation of func()
+                in the random graph, just pass the attributes as a list of strings in this parameter.
+                `ct.WEIGHT` is already passed to the edges of the random graphs, so need to pass it
+                in this parameter
+    **kwargs: keyword arguments if you need to pass them to func()
     """
-    nodesDict = {v:[] for v in G.nodes()}
-    for i in range(n):
-        rand = configuration_model(list(G.degree().values()))
+    if brain.directed:
+        raise TypeError("normalise_node_wise() not available for directed graphs")
+    if init_vals is None:
+        init_vals = func(brain.G, **kwargs)
+    if node_attrs is None:
+        node_attrs = []
+    if edge_attrs is None:
+        edge_attrs = []
+        
+    nodes_dict = {v:[] for v in brain.G.nodes()}
+    for _ in range(n_iter):
+        rand = configuration_model(list(dict(brain.G.degree()).values()))
         rand = nx.Graph(rand) # convert to simple graph from multigraph
 
-        # check whether the graph is weighted
-        # if so, the algorithm randomly reassigns weights from the input graph to the random graph node by node
+        # Reassigning weights from the input graph to the random graph node by node
         # this means that for each node the total weighted degree will be similar
         # nb this depends on the random graph having the same or fewer vertices for each node
-        if isinstance(G.degree(weight=weight)[0], float):
-            randDegs = rand.degree()
-            for node in rand.nodes():
-                edgeVals = [v[2][weight] for v in G.edges(G.nodes()[node],data=True)]
-                shuffle(edgeVals)
-                for en,edge in enumerate(rand.edges(node)):
-                    rand.edge[edge[0]][edge[1]]['weight'] = edgeVals[en]
+        shift = 0 # Some nodes might not exist in brain.G. rand.nodes() will go [0, G.number_of_nodes()]
+        for node in sorted(rand.nodes()):
+            # rand.nodes() have all the values from 0 to brain.G.number_of_nodes() 
+            #  while brain.G.nodes() might not have all the nodes
+            #  Thus, a shift is necessary to map correctly
+            while not brain.G.has_node(node + shift):
+                shift += 1
+            
+            for attr in node_attrs:
+                rand.nodes[node][attr] = brain.G.nodes[node+shift][attr]
+            
+            edge_vals = [v[2] for v in brain.G.edges(node+shift, data=True)]
+            
+            shuffle(edge_vals)
+            for en, edge in enumerate(rand.edges(node)):
+                rand.edges[edge[0], edge[1]][ct.WEIGHT] = edge_vals[en][ct.WEIGHT]
+                for attr in edge_attrs:
+                    rand.edges[edge[0], edge[1]][attr] = edge_vals[en][attr] 
+        
+        # Applying func() to the random graph
+        res = func(rand, **kwargs)
 
-        # remove unconnected nodes
-        nodeList = [rand.degree()[v] for v in rand.nodes() if rand.degree(v)==0]
-        rand.remove_nodes_from(nodeList)
-
-        #        if distance:
-        #            edgeList = [rand.edge[v[0]][v[1]]['weight'] for v in rand.edges() ]
-        #
-        #            # get the maximum edge value, plus any negative correction required
-        #            # and a small correction to keep the values above zero
-        #            # the correction is the inverse of the number of nodes - designed to keep
-        #            # calculations of efficiency sensible
-        #            eMax = np.max(edgeList) + 1/float(len(rand.nodes()))
-        #
-        #            for edge in rand.edges():
-        #                rand.edge[edge[0]][edge[1]]["distance"] = eMax - rand.edge[edge[0]][edge[1]]["weight"] # convert weights to a positive distance
-
-        # add spatial information if necessary
-        if spatial:
-            print("Adding spatial info")
-            nx.set_node_attributes(rand, 'xyz', {rn:G.node[v]['xyz'] for rn,v in enumerate(G.nodes())}) # copy across spatial information
-
-        if distance:
-            res = func(rand, distance=True) # collect values using the function
-        else:
-            try:
-                # collect values using the function
-                # nb it doesn't matter what the specified weight value is (ie weight or distance), the values are stored as 'weight' in the random graph
-                res = func(rand, weight='weight')
-            except:
-                res = func(rand) # collect values using the function
-
-        for x,node in enumerate(nodesDict):
-            nodesDict[node].append(res[x])
-
-    if retNorm: # return the normalised values
-        for node in nodesDict:
-            nodesDict[node] = inVal[node]/np.mean(nodesDict[node])
-        return(nodesDict)
-
-    else: # return a list of the values from the random graph
-        return(nodesDict)
+        for x, node in enumerate(nodes_dict):
+            nodes_dict[node].append(res[x])
+    
+    if ret_normalised: # update nodes_dict to return the normalised values
+        for node in nodes_dict:
+            nodes_dict[node] = init_vals[node]/np.mean(nodes_dict[node])
+    
+    # Returns a list of the values from the random graph
+    return nodes_dict
