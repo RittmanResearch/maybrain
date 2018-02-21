cb0271382b01a1c8e5f63811d4d645157df6b3b9
tjiagoM <tiago.manuel@gmail.com>
2018-02-21 18:08:47 +0000

    tests for normalisation

--- test/test_smoke_plotting_algs.py
+++ test/test_smoke_plotting_algs.py
@@ -1,9 +1,12 @@
 import unittest
 
+import networkx as nx
+
 from maybrain import brain as mbt
 from maybrain import resources as rt
 import maybrain.plotting as mpt
-import maybrain.algorithms as mpa
+import maybrain.algorithms as mba
+import maybrain.constants as ct
 import matplotlib.pyplot as plt
 
 
@@ -15,6 +18,7 @@ class TestPlottingAndAlgs(unittest.TestCase):
     def setUp(self):
         self.a = mbt.Brain()
         self.SMALL_FILE = "test/data/3d_grid_adj.txt"
+        self.SMALL_NEG_FILE = "test/data/3d_grid_adj_neg.txt"
 
     def test_histograms(self):
         self.a.import_adj_file(self.SMALL_FILE)
@@ -35,7 +39,29 @@ class TestPlottingAndAlgs(unittest.TestCase):
     def test_robustness(self):
         self.a.import_adj_file(self.SMALL_FILE)
         self.a.apply_threshold()
-        mpa.robustness(self.a)
+        mba.robustness(self.a)
+
+    def test_normalisation(self):
+        self.a.import_adj_file(self.SMALL_NEG_FILE)
+        self.a.apply_threshold()
+        # Getting random graph
+        while True:
+            try:
+                rand = mba.generate_random_graph_from_degree(self.a, throw_exception=True, edge_attrs=[ct.WEIGHT])
+                break  # if it reaches here, means randomiser didn't throw any exception, so break while
+            except mba.RandomGenerationError as error:
+                pass
+        orig_deg = nx.degree(self.a.G)
+        for n in nx.degree(rand):
+            self.assertEqual(n[1], orig_deg[n[0]])
+
+        normalised = mba.normalise_node_wise(self.a,
+                                             nx.degree,
+                                             init_vals=dict(orig_deg),
+                                             n_iter=5)
+        self.assertTrue(all(i == 1 for i in normalised.values()))
+        self.assertEqual(sum(dict(nx.degree(rand, weight=ct.WEIGHT)).values()),
+                         sum(dict(nx.degree(self.a.G, weight=ct.WEIGHT)).values()))
 
 
 if __name__ == '__main__':
