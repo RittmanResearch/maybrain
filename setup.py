e20d193aff8a383776000aa0fe810cf32af169c0
tjiagoM <tiago.manuel@gmail.com>
2018-03-04 15:17:58 +0000

    correction in parcel information (it was starting with 1, now it starts again with 0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='maybrain',
-    version='0.7.1',
+    version='0.7.2',
     author='Martyn Rittman, Timothy Rittman and Tiago Azevedo',
     author_email='mrittman@physics.org',
     packages=['maybrain', 'maybrain.algorithms', 'maybrain.utils', 'maybrain.plotting', 'maybrain.resources'],
