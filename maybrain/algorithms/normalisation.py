Fixed problem with normalisation - missing nodes not appearing in result

maybrain/algorithms/normalisation.py
@@ -261,9 +261,10 @@ def normalise(brain, func, init_vals=None, n_iter=500, ret_normalised=True, exac
         res = func(rand, **kwargs)
 
         if isinstance(init_vals, dict):
+            # convert results to a dictionary if not already so
             if not isinstance(res, dict):
                 res = {v[0]:v[1] for v in res}
-            for node in nodes_dict:
+            for node in res:
                 nodes_dict[node].append(res[node])
         else:
             vals.append(res)
@@ -271,7 +272,7 @@ def normalise(brain, func, init_vals=None, n_iter=500, ret_normalised=True, exac
     if isinstance(init_vals, dict):
         if ret_normalised:  # update nodes_dict to return the normalised values
             for node in nodes_dict:
-                nodes_dict[node] = init_vals[node] / np.mean(nodes_dict[node])
+                nodes_dict[node] = init_vals[node] / np.nanmean(nodes_dict[node])
         return nodes_dict
 
     if ret_normalised: